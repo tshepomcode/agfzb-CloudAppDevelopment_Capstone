0fca176eed46699598de044945f762094715135a
TshepoMCode <tshepomcode@gmail.com>
2022-03-08 06:11:31 +0200

    Models redone

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from django.db import models
 from django.forms import DateField
 from django.utils.timezone import now
@@ -10,13 +11,14 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+
 class CarMake(models.Model):
-  name = models.CharField(max_length=30)
+  name = models.CharField(max_length=50)
   description = models.CharField(max_length=255)
 
   def __str__(self) -> str:
+      return f'Name {self.name} \nDescription{self.description}'
 
-      return f'CarMake({self.name},{self.description})'
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -26,37 +28,31 @@ class CarMake(models.Model):
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
-class CarModel(models.Models):
+class CarModel(models.Model):
+  SEDAN = 'sedan'
   SUV = 'SUV'
-  SEDAN = 'SDN'
-  WAGON = 'WGN'
-  COUPE = 'CPE'
-  HATCHBACK = 'HTB'
-  MINIVAN = 'MVN'
-  CONVERTIBLE = 'CNE'
+  WAGON = 'WAGON'
+  COUPE = 'COUPE'
   CAR_TYPE_CHOICES = [
-    ('SUV', 'SMALL UTILITY VEHICLE'),
-    ('SDN', 'Sedan'),
-    ('WGN', 'Station Wagon'),
-    ('CPE', 'Coupe'),
-    ('HTB', 'Hatchback'),
-    ('MVN', 'Minivan'),
-    ('CNE', 'Convertible'),
+    (SEDAN, 'Sedan'),
+    (SUV, 'SUV'),
+    (WAGON, 'Wagon'),
+    (COUPE, 'Coupe')
   ]
   car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-  name = models.CharField(max_length=30)
+  name = models.CharField(max_length=50)
   dealer_id = models.IntegerField()
   car_type = models.CharField(
-    max_length=3,
+    max_length=10,
     choices=CAR_TYPE_CHOICES,
     default=SEDAN
   )
-  year = DateField('date made')
+  year = models.DateField()
+
 
   def __str__(self) -> str:
-      return f'CarMake({self.car_make}, {self.name}, {self.dealer_id}, {self.car_type}, {self.year})'
-
-
+      return f'Name: {self.name} Dealer ID: {self.dealer_id} Car Type: {self.car_type}' \
+        + f'Year: {self.year}'
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
