0fe60d376b632905e5bd3d64e2c4d5197423245b
TshepoMCode <tshepomcode@gmail.com>
2022-03-23 04:57:44 +0200

    nlu updated with results

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -125,11 +125,28 @@ def get_dealer_by_id_from_cf(url, dealerId):
             dealer_doc = dealer
             if key in dealer_doc.keys():
                 sentiment = analyze_review_sentiments(dealer_doc["review"])
-                print(sentiment)
+                print(f'sentiment = {sentiment}')
+                dealership = dealer_doc["dealership"]
+                name = dealer_doc["name"]
+                purchase = dealer_doc["purchase"]
+                review = dealer_doc["review"]
+                if dealer_doc['purchase'] == True:
+                    purchase_date = dealer_doc["purchase_date"]
+                    car_make = dealer_doc["car_make"]
+                    car_model = dealer_doc["car_model"]
+                    car_year = dealer_doc["car_year"]
+                    id = dealer_doc["id"]
+                else:
+                    purchase_date = ''
+                    car_make = ''
+                    car_model = ''
+                    car_year = ''
 
                 # Create a DealerReview object with values in `doc` object
-                dealer_review_obj = DealerReview(dealership=dealer_doc["dealership"], name=dealer_doc["name"],
-                                                 purchase=dealer_doc["purchase"], review=dealer_doc["review"]
+                dealer_review_obj = DealerReview(dealership=dealership, name=name, purchase=purchase,
+                                                 review=review, purchase_date=purchase_date, car_make=car_make,
+                                                 car_model=car_model,car_year=car_year, sentiment=sentiment,
+                                                 id = id
                                                  )
                 # if dealer_doc['purchase'] == True:
                 #     dealer_review_obj.id = dealer_doc["id"]
@@ -138,7 +155,7 @@ def get_dealer_by_id_from_cf(url, dealerId):
                 #     dealer_review_obj.car_model = dealer_doc["car_model"]
                 #     dealer_review_obj.car_year = dealer_doc["car_year"]
 
-                dealer_review_obj.sentiment = sentiment
+                # dealer_review_obj.sentiment = sentiment
                 results.append(dealer_review_obj)
 
     return results
@@ -149,8 +166,15 @@ def get_dealer_by_id_from_cf(url, dealerId):
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
 def analyze_review_sentiments(dealerreview):
-    url = 'https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/76981f41-32f6-469d-b016-998f82f81345/v1/analyze'
-    api_key = 'HhX42guzXHFX_G8VinROszwYQ1ajlGVfCfn9ywAvS3UI'
+    from ibm_watson import NaturalLanguageUnderstandingV1
+    from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+    from ibm_watson.natural_language_understanding_v1 import Features, SentimentOptions, EntitiesOptions, KeywordsOptions
+    from ibm_watson import AssistantV1, ApiException
+
+    with open('/etc/nlu_api_key.txt') as f:
+        api_key = f.read().strip()
+
+    url = 'https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/76981f41-32f6-469d-b016-998f82f81345'
 
     params = dict()
     params['api_key'] = api_key
@@ -159,6 +183,25 @@ def analyze_review_sentiments(dealerreview):
     params['features'] = 'sentiment'
     params['return_analyzed_text'] = True
     params['language'] = 'en'
-    response = get_request(url, kwargs=params)
 
-    return response
+    authenticator = IAMAuthenticator(api_key)
+    natural_language_understanding = NaturalLanguageUnderstandingV1(
+        version=params['version'],
+        authenticator=authenticator
+    )
+
+    natural_language_understanding.set_service_url(url)
+    response = natural_language_understanding.analyze(
+        text=dealerreview,
+        features=Features(
+            sentiment=SentimentOptions(targets=[dealerreview])
+            ),
+        language='en').get_result()
+    label = json.dumps(response, indent=2)
+    label = response['sentiment']['document']['label']
+
+    # print(json.dumps(response, indent=2))
+
+    # response = get_request(url, kwargs=params)
+
+    return label
